651f82be6bf736610cc07ee9f6e50ee2fa0834a9
Nishanthini1203 <nishanthinigani@gmail.com>
2025-06-10 13:19:47 +0530

    for fetch url update in studentdata of admin

--- src/Admin/Function/Studentsdata.jsx
+++ src/Admin/Function/Studentsdata.jsx
@@ -160,7 +160,7 @@ const Studentsdata = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:5000/studentsdataforadmin")
+    fetch("https://placeme-database-backend.onrender.com/studentsdataforadmin")
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch data");
         return res.json();
